ce10f34edd535d076a9b9a6b7ec55ce069c87ae2
Andrew <andrew.neupane@alphaventus.com>
2020-11-19 02:27:23 +0545

    edit invoice changes

---
+++ src/Accounts.Application/Invoicing/Dto/UpdateInvoiceInputDto.cs
@@ -0,0 +1,20 @@
+﻿using Accounts.HourLogEntries.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accounts.Invoicing.Dto
+{
+    public class UpdateInvoiceInputDto
+    {
+        public InvoiceDto Invoice { get; set; }
+        public IEnumerable<UpdatedHourLogEntries> UpdatedHourLogEntries { get; set; }
+    }
+
+    public class UpdatedHourLogEntries
+    {
+        public double? Hours { get; set; }
+        public DateTime Day { get; set; }
+        public int? ProjectId { get; set; }
+    }
+}
